c40d799a0b1663f869cd80cbadeed6cf6bf3eeb5
Ajay <ajaydeepak07@in.ibm.com>
2018-10-15 19:27:52 +0530

    Classes with constructors

--- src/Parameters.kt
+++ src/Parameters.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>){
 
     // not giving the 2nd argument separator as it is already has default parameters values in function declaration
     val value1 = concat(listOf("kotlin", "Java", "Bentely"))
+
     // different orders of arguments can be used as long as arguments types are mentioned
     val value = concat(separator=":", words = listOf("kotlin", "Java", "Bentely"))
     print("$value")
